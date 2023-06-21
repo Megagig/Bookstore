f97b713b061ac9505a282a7b5c3111320854d3cf
ANTHONY OBI <Megagigsoftwaresolutions@gmail.com>
2023-06-21 16:36:46 +0100

    Import BookInfo Component

--- src/components/Booklist.jsx
+++ src/components/Booklist.jsx
@@ -1,33 +1,11 @@
-// import React from 'react';
-import PropTypes from 'prop-types';
+import BookInfo from './BookInfo';
 import BookItem from './BookItem';
 
-const BookList = ({ books, handleRemoveBook }) => (
-  <div className="book-container">
-    {books.length === 0 ? (
-      <p>Book list is empty</p>
-    ) : (
-      books.map((book) => (
-        <BookItem
-          key={book.item_id}
-          book={book}
-          handleRemoveBook={handleRemoveBook}
-        />
-      ))
-    )}
+const Books = () => (
+  <div>
+    <BookItem />
+    <BookInfo />
   </div>
 );
 
-BookList.propTypes = {
-  books: PropTypes.arrayOf(
-    PropTypes.shape({
-      item_id: PropTypes.string.isRequired,
-      title: PropTypes.string.isRequired,
-      author: PropTypes.string.isRequired,
-      category: PropTypes.string.isRequired,
-    })
-  ).isRequired,
-  handleRemoveBook: PropTypes.func.isRequired,
-};
-
-export default BookList;
+export default Books;
