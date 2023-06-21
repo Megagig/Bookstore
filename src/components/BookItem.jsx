58e61837c78921e055b8574a2e878ef809e413ad
ANTHONY OBI <Megagigsoftwaresolutions@gmail.com>
2023-06-21 16:53:01 +0100

    Add functionality for removing books

--- src/components/BookItem.jsx
+++ src/components/BookItem.jsx
@@ -1,29 +1,68 @@
-// import React from 'react';
-import PropTypes from 'prop-types';
+import React, { useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { fetchBooks, removeBook } from '../redux/bookSlice';
 
-const BookItem = ({ book, handleRemoveBook }) => (
-  <div>
-    <div>
-      <p>{book.category}</p>
-      <h3>{book.title}</h3>
-      <p>{book.author}</p>
-    </div>
-    <div>
-      <button type="button" onClick={() => handleRemoveBook(book.item_id)}>
-        Remove
-      </button>
-    </div>
-  </div>
-);
+const BookItem = () => {
+  const dispatch = useDispatch();
+  const { books, isLoading, error } = useSelector((state) => state.books);
 
-BookItem.propTypes = {
-  book: PropTypes.shape({
-    item_id: PropTypes.string.isRequired,
-    title: PropTypes.string.isRequired,
-    author: PropTypes.string.isRequired,
-    category: PropTypes.string.isRequired,
-  }).isRequired,
-  handleRemoveBook: PropTypes.func.isRequired,
+  useEffect(() => {
+    dispatch(fetchBooks());
+  }, [dispatch]);
+
+  if (isLoading) {
+    return <p>Loading...</p>;
+  }
+
+  if (error) {
+    return <p>Could not fetch data.</p>;
+  }
+
+  if (!books) {
+    return null;
+  }
+
+  const renderBook = (itemId, book) => {
+    const { category, title, author, chapter } = book[0];
+
+    const handleRemoveBook = () => {
+      dispatch(removeBook(itemId));
+    };
+
+    return (
+      <div key={itemId}>
+        <div>
+          <p>{category}</p>
+          <h3>{title}</h3>
+          <p>{author}</p>
+          <div>
+            <button type="button">Comment</button>
+            <div>
+              <button type="button" onClick={handleRemoveBook}>
+                Remove
+              </button>
+            </div>
+            <button type="button">Edit</button>
+          </div>
+        </div>
+        <div>
+          <h4>75%</h4>
+          <div>Completed</div>
+        </div>
+        <div>
+          <h3>Current Chapter</h3>
+          <p>{chapter}</p>
+          <button type="button">Update Progress</button>
+        </div>
+      </div>
+    );
+  };
+
+  return (
+    <div>
+      {Object.entries(books).map(([itemId, book]) => renderBook(itemId, book))}
+    </div>
+  );
 };
 
 export default BookItem;
