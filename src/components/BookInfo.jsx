ad51828b8b15416eee27fc0192a9cce41c946099
ANTHONY OBI <Megagigsoftwaresolutions@gmail.com>
2023-06-22 08:31:46 +0100

    Fix linters bug

--- src/components/BookInfo.jsx
+++ src/components/BookInfo.jsx
@@ -1,5 +1,4 @@
-/* eslint-disable no-unused-vars */
-import React, { useState } from 'react';
+import { useState } from 'react';
 import { useDispatch } from 'react-redux';
 import { nanoid } from 'nanoid';
 import { addBook } from '../redux/books/bookSlice';
