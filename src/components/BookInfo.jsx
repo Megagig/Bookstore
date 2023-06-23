7f7e17c53d1841b279841a829d47d271025519b6
ANTHONY OBI <Megagigsoftwaresolutions@gmail.com>
2023-06-23 09:44:30 +0100

    Installed required packages and dependencies

--- src/components/BookInfo.jsx
+++ src/components/BookInfo.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react';
 import { useDispatch } from 'react-redux';
 import { nanoid } from 'nanoid';
 import { addBook, fetchBooks } from '../redux/books/bookSlice';
+import '../styles/BookInfo.css';
 
 const NewBooks = () => {
   const dispatch = useDispatch();
@@ -44,39 +45,42 @@ const NewBooks = () => {
 
   return (
     <>
-      <form>
-        <h3>Add a new book...</h3>
-        <input
-          type="text"
-          name="title"
-          placeholder="Title"
-          value={title}
-          onChange={handleInputChange}
-        />
-        <input
-          type="text"
-          name="author"
-          placeholder="Author"
-          value={author}
-          onChange={handleInputChange}
-        />
-        <select name="category" value={category} onChange={handleInputChange}>
-          <option value="">Select Category</option>
-          <option value="Memoir">Religion</option>
-          <option value="Science">Technology</option>
-          <option value="Travel">Education</option>
-          <option value="Business/Finance">Business</option>
-          <option value="Poetry">Finance</option>
-          <option value="Science Fiction">Non-fiction</option>
-          <option value="Horror">Fiction</option>
-          <option value="Comedy/Humor">Agriculture</option>
-          <option value="Drama/Play">Music</option>
-          <option value="Novels/Comics">Drama</option>
-          <option value="Cookbooks">Science</option>
-        </select>
-        <button type="button" onClick={addBookHandler}>
-          Add Book
-        </button>
+      <form className="form-holder">
+        <hr />
+        <h3 className="heading">Add a new book...</h3>
+        <div className="material">
+          <input
+            type="text"
+            name="title"
+            placeholder="Title"
+            value={title}
+            onChange={handleInputChange}
+          />
+          <input
+            type="text"
+            name="author"
+            placeholder="Author"
+            value={author}
+            onChange={handleInputChange}
+          />
+          <select name="category" value={category} onChange={handleInputChange}>
+            <option value="">Select Category</option>
+            <option value="Memoir">Religion</option>
+            <option value="Science">Technology</option>
+            <option value="Travel">Education</option>
+            <option value="Business/Finance">Business</option>
+            <option value="Poetry">Finance</option>
+            <option value="Science Fiction">Non-fiction</option>
+            <option value="Horror">Fiction</option>
+            <option value="Comedy/Humor">Agriculture</option>
+            <option value="Drama/Play">Music</option>
+            <option value="Novels/Comics">Drama</option>
+            <option value="Cookbooks">Science</option>
+          </select>
+          <button type="button" onClick={addBookHandler}>
+            Add Book
+          </button>
+        </div>
       </form>
     </>
   );
